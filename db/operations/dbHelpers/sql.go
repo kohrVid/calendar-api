4a4f2bd92673fb0c854333d26d01292b4da80616
kohrVid <kohrVid@zoho.com>
2020-02-16 10:31:55 +0000

    Fix db-clean command
    The db-clean command currently deleted the go-pg migrations table which
    breaks the db-migrate command. Have fixed the truncate command so that
    it ignores this table going forwards.

--- db/operations/dbHelpers/sql.go
+++ db/operations/dbHelpers/sql.go
@@ -16,7 +16,9 @@ CREATE OR REPLACE FUNCTION truncate_tables(username IN VARCHAR) RETURNS void AS 
 DECLARE
     statements CURSOR FOR
         SELECT tablename FROM pg_tables
-        WHERE tableowner = username AND schemaname = 'public';
+        WHERE tableowner = username
+	  AND schemaname = 'public'
+	  AND tablename != 'gopg_migrations';
 BEGIN
     FOR stmt IN statements LOOP
         EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' RESTART IDENTITY CASCADE;';
